33c9d63b57342ea65eb21551c43c8d8179065825
twengee <wengee@u.nus.edu>
2024-04-05 13:56:16 +0800

    add in css

---
+++ jaws/src/component/ProgressCircle/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { FC } from "react";
+import { DatePicker } from "antd";
+
+export const ProgressCircle: FC = () => {
+    return (<div><DatePicker/></div>)
+}
--- jaws/src/pages/mainPage.tsx
+++ jaws/src/pages/mainPage.tsx
@@ -1,28 +1,30 @@
-import { FC } from "react"
-import { TestDatePicker } from "../component/TestDatePicker"
-import { Layout } from "antd"
+import { FC } from "react";
+import { TestDatePicker } from "../component/TestDatePicker";
+import { Layout } from "antd";
+import { Profile } from "../component/Profile";
+import { ProgressCircle } from "../component/ProgressCircle";
 
-const { Header, Content, Footer} = Layout;
+const { Content, Footer } = Layout;
 
 export const MainPage: FC = () => {
-    return (
-    <Layout style={{minHeight: "100vh"}}>
-        <Header style={{backgroundColor: "#00b0b2"}}>
-            <p>
-            header
-            </p>
-        </Header>
-        <Content>
-            <p>
-           content
-           <TestDatePicker/>
-            </p>
-        </Content>
-        <Footer>
-            <p>
-           footer
-            </p>
-        </Footer>
+  return (
+    <Layout
+      style={{
+        minHeight: "100vh",
+        backgroundImage: "linear-gradient(#ffffff, #7fb9ba)",
+      }}
+    >
+      <div style={{justifyContent: "left", display:'flex', padding: "10px"}}>
+        <Profile />
+      </div>
+      <Content>
+        <div>
+            <ProgressCircle/>
+        </div>
+      </Content>
+      <Footer>
+        <p>footer</p>
+      </Footer>
     </Layout>
-  )
-}
+  );
+};
